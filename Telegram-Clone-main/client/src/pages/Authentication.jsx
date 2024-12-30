cc7f02f66c975b3cad319259cba35d80651b446a
NuhDemir <drmaa217@gmail.com>
2024-12-30 12:00:25 +0300

    auth

--- Telegram-Clone-main/client/src/pages/Authentication.jsx
+++ Telegram-Clone-main/client/src/pages/Authentication.jsx
@@ -5,7 +5,6 @@ import Register from "../components/pages/Authentication/Register";
 
 function Authentication() {
   const [userWantToLogin, setUserWantsToLogin] = useState(true);
-
   return (
     <div className="chat-bg w-full h-full flex items-center justify-center px-[1rem]">
       {userWantToLogin && <Login setUserWantsToLogin={setUserWantsToLogin} />}
